f30ecfeea372f5339a59e5454f2bd56edb4c3588
rananischal15@gmail.com <rananischal15@gmail.com>
2021-08-02 22:12:53 +1000

    again

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 const http = require("http");
 const { userInfo } = require("os");
